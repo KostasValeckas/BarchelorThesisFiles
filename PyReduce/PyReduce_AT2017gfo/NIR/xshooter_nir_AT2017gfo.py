908ee100a1d768074fd2b2ede3262a9958d2cada
Kostas <cpf125@alumni.ku.dk>
2023-05-06 15:49:36 +0200

    PyReduce thesis version runs

---
+++ PyReduce/PyReduce_AT2017gfo/NIR/xshooter_nir_AT2017gfo.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Simple usage example for PyReduce
+Loads a sample X-shooter dataset, and runs the full extraction
+"""
+
+import os.path
+
+import pyreduce
+from pyreduce import datasets
+
+# define parameters
+instrument = "XSHOOTER"
+target = ".*"
+night = None
+mode = "NIR"
+steps = (
+     "bias",
+     "flat",
+     "orders",
+     "scatter",
+     "norm_flat",
+     "curvature",
+     "wavecal",
+     "science",
+    # "continuum",
+    # "finalize",
+)
+base_dir = "."
+input_dir = "NIR"
+output_dir = "OUTPUT"
+
+config = pyreduce.configuration.get_configuration_for_instrument(instrument, plot=1)
+#config["science"]["extraction_method"] = "arc"
+#config["science"]["extraction_cutoff"] = 0
+
+pyreduce.reduce.main(
+    instrument,
+    target,
+    night,
+    mode,
+    steps,
+    base_dir=base_dir,
+    input_dir=input_dir,
+    output_dir=output_dir,
+    configuration=config,
+    #order_range=(0,15),
+)
--- binned data/readme.txt
+++
@@ -1,9 +0,0 @@
-Rebinned data used to test PypeIt and PyReduce on GW170817  X-Shooter data, for different dates of observation. 
-
-The rebinning script can be found in this repository, search for rebinner.py
-
-The raw data can be found at:
-
-NIR: https://1drv.ms/u/s!AnOeEPXP7Y4fgo1kI14uXLubOj8VlA?e=awhR8t
-VIS: https://1drv.ms/u/s!AnOeEPXP7Y4fgpBLsU_xQ8UngIoNYw?e=5xB1mw
-UVB: https://1drv.ms/u/s!AnOeEPXP7Y4fgpFoNJpRh7YilflReg?e=AYHvhg
